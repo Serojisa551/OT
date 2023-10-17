594b132927eedfeaa2f005889f77eefe6369f4cf
Serojisa551 <116907048+Serojisa551@users.noreply.github.com>
2023-10-17 16:03:14 +0400

    - Added user authentication with secure credentials validation. - Displayed the username on the main page after successful login. - Included a "Login" and "Logout" option in the user interface.

--- OTMS/register/urls.py
+++ OTMS/register/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path("", main_page, name="main_page"),
     path("register/", register_users, name="register_users"),
-    
+    path("authentic/", authentic_user, name="authentic_user"),
+    path("logout/", logout_request, name="logout_request"),
 ]
--- OTMS/register/views.py
+++ OTMS/register/views.py
@@ -1,12 +1,15 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User 
 
 def main_page(request):
     """
     Displays the main page of your web application.
     """
-    return render(request, 'global/main.html')
+    user = request.user
+    return render(request, 'global/main.html', {'users': user})
 
 def register_users(request):
     """
@@ -24,3 +27,31 @@ def register_users(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'register/register_users.html', {'form': form})
+
+def authentic_user(request):
+    """
+    This function processes user authentication. It takes a POST request with user credentials
+    and validates them using the provided form. If the form is valid and the credentials are correct,
+    the user is logged in and redirected to the main page of the application. If the credentials are
+    incorrect, an error message is displayed on the login page.
+    """
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)  
+                return redirect("main_page")
+            else:
+                messages.error(request, "Invalid username or password.") 
+    form = AuthenticationForm()
+    return render(request, "register/login_users.html", {"login_form": form})
+
+def logout_request(request):
+    """
+    Logs out the currently logged-in user and redirects to the login page.
+    """
+    logout(request) 
+    return redirect('main_page')
